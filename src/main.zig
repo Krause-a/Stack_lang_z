2a50aff510f7481333b519e2a6139084b7f95756
Krause-a <xander876@gmail.com>
2024-07-24 08:52:18 -0700

    Add duplicate and repeat functions

--- src/main.zig
+++ src/main.zig
@@ -24,7 +24,9 @@ pub fn main() !void {
             break;
         }
         if (eq(input, "help") or eq(input, "h")) {
-            // TODO: add a help mapping for all functions
+            try printHelp(stdout);
+            try bw.flush(); // don't forget to flush!
+            continue;
         }
 
         const tokens = try tokenize(gpa, input);
@@ -37,9 +39,45 @@ pub fn main() !void {
     try bw.flush(); // don't forget to flush!
 }
 
+fn printHelp(writer: anytype) !void {
+    try writer.print("plus: pop 2 values from the value stack. Add them and put that value into the value stack.\n", .{});
+    try writer.print("minus: pop 2 values from the value stack. Subtract them and put that value into the value stack.\n", .{});
+    try writer.print(".: pop and execute the top function from the function stack.\n", .{});
+    try writer.print("[[:digit:]]: a value to be put on the value stack.\n", .{});
+    try writer.print("\n", .{});
+}
+
+const StackType = enum {
+    Func,
+    Value,
+};
+
+const StackPopCount = struct {
+    func: u8,
+    value: u8,
+};
+
 const FuncType = enum {
     Plus,
     Minus,
+    DuplicateValue,
+    DuplicateFunc,
+    Repeat,
+
+    fn getPopCount(self: FuncType) StackPopCount {
+        var count_func: u8 = 0;
+        var count_value: u8 = 0;
+        switch (self) {
+            .Plus, .Minus => count_value = 2,
+            .DuplicateValue => count_value = 1,
+            .DuplicateFunc => count_func = 1,
+            .Repeat => {
+                count_value = 1;
+                count_func = 1;
+            },
+        }
+        return StackPopCount{ .func = count_func, .value = count_value };
+    }
 };
 
 const Token = union(enum) {
@@ -67,9 +105,17 @@ fn tokenize(allocator: anytype, string: []const u8) ![]Token {
     if (string_buffer.items.len > 0) {
         try tokens.append(parseToken(string_buffer.items));
     }
-    // TODO: Reverse the order of these items!
-    // It might exist already as a function, otherwise make my own!
+    reverse(tokens.items);
     return tokens.items;
+}
+
+fn reverse(slice: anytype) void {
+    var i: u64 = 0;
+    while (i < slice.len / 2) : (i += 1) {
+        const temp = slice[i];
+        slice[i] = slice[slice.len - 1 - i];
+        slice[slice.len - 1 - i] = temp;
+    }
 }
 
 fn eq(a: []u8, b: anytype) bool {
@@ -78,7 +124,13 @@ fn eq(a: []u8, b: anytype) bool {
 
 fn parseToken(single_token_string: []u8) Token {
     //std.debug.print("single_token_string: {s}\n", .{single_token_string});
-    if (eq(single_token_string, ".")) {
+    if (eq(single_token_string, "repeat")) {
+        return Token{ .func = FuncType.Repeat };
+    } else if (eq(single_token_string, "duplicatev")) {
+        return Token{ .func = FuncType.DuplicateValue };
+    } else if (eq(single_token_string, "duplicatef")) {
+        return Token{ .func = FuncType.DuplicateFunc };
+    } else if (eq(single_token_string, ".")) {
         return Token.execute;
     } else if (eq(single_token_string, "+") or eq(single_token_string, "plus") or eq(single_token_string, "add")) {
         return Token{ .func = FuncType.Plus };
@@ -107,7 +159,7 @@ fn executeStack(allocator: anytype, tokens: []const Token) ![]u8 {
             .empty => {},
             .execute => {
                 const func = stack_func.pop();
-                try executeFunc(func, &stack_func, &stack_value);
+                try executeFunc(allocator, func, &stack_func, &stack_value);
             },
         }
     }
@@ -156,8 +208,7 @@ fn tokenToString(allocator: anytype, token: Token) ![]u8 {
     return list.items;
 }
 
-fn executeFunc(func: FuncType, stack_func: *std.ArrayList(FuncType), stack_value: *std.ArrayList(i64)) !void {
-    _ = stack_func;
+fn executeFunc(allocator: anytype, func: FuncType, stack_func: *std.ArrayList(FuncType), stack_value: *std.ArrayList(i64)) !void {
     switch (func) {
         .Plus => {
             const a = stack_value.pop();
@@ -168,6 +219,54 @@ fn executeFunc(func: FuncType, stack_func: *std.ArrayList(FuncType), stack_value
             const a = stack_value.pop();
             const b = stack_value.pop();
             try stack_value.append(a - b);
+        },
+        .DuplicateFunc => {
+            const a = stack_func.pop();
+            try stack_func.append(a);
+            try stack_func.append(a);
+        },
+        .DuplicateValue => {
+            const a = stack_value.pop();
+            try stack_value.append(a);
+            try stack_value.append(a);
+        },
+        .Repeat => {
+            // TODO: Duplicatef works wrong when combined with repeat.
+            var repeat_count = stack_value.pop();
+            if (repeat_count > 1) {
+                const repeat_func = stack_func.pop();
+                var list_f = std.ArrayList(FuncType).init(allocator);
+                // var list_v = std.ArrayList(i64).init(allocator);
+                // Take them both off. put them back on exe, put them back on exe...
+                var pop_count = repeat_func.getPopCount();
+
+                while (pop_count.func > 0) : (pop_count.func -= 1) {
+                    const f = stack_func.pop();
+                    const pop_count_each = f.getPopCount();
+                    pop_count.func += pop_count_each.func;
+                    pop_count.value += pop_count_each.value;
+
+                    try list_f.append(f);
+                }
+
+                // while (pop_count.value > 0) : (pop_count.value -= 1) {
+                //     try list_v.append(stack_value.pop());
+                // }
+                reverse(list_f.items);
+                // reverse(list_v.items);
+
+                while (repeat_count > 0) : (repeat_count -= 1) {
+                    for (list_f.items) |func_f| {
+                        try stack_func.append(func_f);
+                    }
+                    // for (list_v.items) |func_v| {
+                    //     try stack_value.append(func_v);
+                    // }
+                    try executeFunc(allocator, repeat_func, stack_func, stack_value);
+                }
+            } else {
+                try executeFunc(allocator, stack_func.pop(), stack_func, stack_value);
+            }
         },
     }
 }
